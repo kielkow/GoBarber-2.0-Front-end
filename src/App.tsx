8de5e0c9bd0113ee79df0caa623e1c80ae14bf79
kielkow <matheuskiel@fiorifer.com.br>
2020-05-02 03:22:30 -0300

    Created toast hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastConatiner from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-
-    <ToastConatiner />
+    </AppProvider>
 
     <GlobalStyle />
   </>
