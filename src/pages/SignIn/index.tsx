5416665899a887eb8aa7cc4e50c036d8222b3625
kielkow <matheuskiel@fiorifer.com.br>
2020-05-02 16:20:04 -0300

    Add and remove toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -53,7 +53,11 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Authentication error',
+          description: 'Error on application sign in, check your credentials.',
+        });
       }
     },
     [signIn, addToast],
